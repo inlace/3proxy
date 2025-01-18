b0f769bc318df1c3034edaaca49cb1ea560d182d
inlace <31384129+inlace@users.noreply.github.com>
2025-01-18 14:41:53 +0700

    Update 3proxyinstall.sh

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -8,16 +8,5 @@ sudo make -f Makefile.Linux
 sudo mkdir /etc/3proxy
 cd ~/3proxy-0.9.3/bin
 sudo cp 3proxy /usr/bin/
-sudo nano /etc/3proxy/3proxy.cfg
-sudo nano /etc/3proxy/.proxyauth
-sudo chown root -R /etc/3proxy
-sudo chown root /usr/bin/3proxy
-sudo chmod 777 /etc/3proxy/3proxy.cfg
-sudo chmod 777 /etc/3proxy/.proxyauth
-sudo mkdir /var/log/3proxy
-sudo nano /etc/systemd/system/3proxy.service
-sudo systemctl daemon-reload
-sudo systemctl enable 3proxy
-sudo ufw allow 3128/tcp
-sudo ufw enable
-sudo systemctl start 3proxy
+sudo adduser --system --no-create-home --disabled-login --group proxy3
+id proxy3
